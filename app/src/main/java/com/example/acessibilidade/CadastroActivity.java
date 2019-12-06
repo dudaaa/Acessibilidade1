a93142573a521953835ccda12898bfc6256ff1ae
dudaaa <celso.a.o.eduardo@gmail.com>
2019-12-06 12:40:50 -0300

    f

--- app/src/main/java/com/example/acessibilidade/CadastroActivity.java
+++ app/src/main/java/com/example/acessibilidade/CadastroActivity.java
@@ -8,8 +8,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.DebugUtils;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -20,24 +24,32 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     public void mudarTela (View v) {
+        String nome = ((TextView)findViewById(R.id.editTextNome)).getText().toString();
+        String email = ((TextView)findViewById(R.id.editTextEmail)).getText().toString();
+        String senha = ((TextView)findViewById(R.id.editTextSenha)).getText().toString();
+
+        try {
+            DBHelper.insertIntoDadosUsuario(nome, email, senha);
+        } catch (IOException e) {}
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Bem vindo NOME");
+        builder.setTitle("Bem vindo");
         builder.setMessage("Seu cadastro foi efetuado com sucesso!");
 
         DialogInterface.OnClickListener btnOk = new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 SharedPreferences sp = getSharedPreferences("infos_login", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sp.edit();
-                editor.putString("email", email);
-                editor.putString("nome", nome);
+                editor.putString("email", null);
+                editor.putString("nome", null);
                 Intent intent = new Intent(getApplicationContext(), TelaInicialActivity.class);
                 startActivity(intent);
             }
         };
+
         builder.setPositiveButton("Ok", btnOk);
         builder.create().show();
     }
-
 }
 
 
--- app/src/main/java/com/example/acessibilidade/DBHelper.java
+++ app/src/main/java/com/example/acessibilidade/DBHelper.java
@@ -2,6 +2,10 @@ package com.example.acessibilidade;
 
 import android.os.Build;
 import android.os.StrictMode;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,24 +15,28 @@ import java.net.URL;
 
 public class DBHelper {
 
-    public static int insertIntoDadosUsuario (String nome, String email) throws IOException{
+    private static String WEB_SERVICE_URL =  "http://10.21.80.171/web_service/";
+
+    private static void checkThreadPolicy(){
+        int version = Build.VERSION.SDK_INT;
+        if(version > 8){
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+    }
+
+    public static int insertIntoDadosUsuario (String nome, String email, String senha) throws IOException {
         checkThreadPolicy();
-        String values = "nome=" +nome+ "&" + "email=" +  email;
-        URL url = new URL (WEB_SERVICE_URL + "ws_insert/ws_insert_usuarios.php" + values);
+        String values = "nome=" +nome+ "&" + "email=" +  email + "&" + "senha=" + senha;
+        URL url = new URL (WEB_SERVICE_URL + "ws_insert/ws_insert_usuarios.php?" + values);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         BufferedReader bufferedReader = new BufferedReader (new InputStreamReader(con.getInputStream()));
         String response = bufferedReader.readLine();
-        if (response.equals("False")){
-            return 0;
-        } else {
+        if (response.equals("true")){
             return 1;
-        }
-    }
-    private static void checkThreadPolicy(){
-        int SDK_INT = Build.VERSION.SDK_INT;
-        if (SDK_INT > 8) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+        } else {
+            return 0;
         }
     }
 }
